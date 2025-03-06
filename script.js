8dce4e0a68d6f5b786d7dc14b1dfdad2eef0add8
hassan khaled <hasan.khaled.fcis@gmail.com>
2025-03-06 13:33:38 +0200

    feat(add_egypt_api_request): make the egypt main focus on the extension
     filder by date and by country using querystring

--- script.js
+++ script.js
@@ -33,13 +33,46 @@ chrome.action.onClicked.addListener(async (tab) => {
 });
 
 async function fetchData() {
-    const res = await fetch("https://covid-api.com/api/reports/total");
+    const res = await fetch("https://covid-api.com/api/reports??date=2020-04-16&iso=EGY");
     const record = await res.json();
+    const data = record.data[0];
+    console.log(data.date);
 
-    document.getElementById("date").value = record.data.date;
-    document.getElementById("areaName").value = record.data.active;
-    document.getElementById("latestBy").value = record.data.deaths;
-    document.getElementById("deathNew").value = record.data.confirmed;
+    document.getElementById("country").innerHTML = data.region.name;
+    document.getElementById("date").value = data.date;
+    document.getElementById("active").value = data.active;
+    document.getElementById("deathNew").value = data.deaths;
+    document.getElementById("confirmed").value = data.confirmed;
+
+
+
+    /* 
+    data": [
+    {
+    "date": "2020-04-16",
+    "confirmed": 2673,
+    "deaths": 196,
+    "recovered": 596,
+    "confirmed_diff": 168,
+    "deaths_diff": 13,
+    "recovered_diff": 7,
+    "last_update": "2020-04-16 23:30:31",
+    "active": 1881,
+    "active_diff": 148,
+    "fatality_rate": 0.0733,
+    "region": {
+    "iso": "EGY",
+    "name": "Egypt",
+    "province": "",
+    "lat": "26.8206",
+    "long": "30.8025",
+    "cities": []
+    }
+    }
+    ]
+    
+    
+    */
 
 
     // document.getElementById("date").innerHTML = "12-9-2025";
