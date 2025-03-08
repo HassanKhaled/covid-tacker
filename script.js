125b22466329c80fdd9d00f8ec40ab5c758bfe74
hassan khaled <hasan.khaled.fcis@gmail.com>
2025-03-08 16:57:39 +0200

    chore(remove_comments) remove comments in script file
     make code more clear and easy to understand

--- script.js
+++ script.js
@@ -28,40 +28,5 @@ async function fetchData() {
     document.getElementById("confirmed").value = data.confirmed;
 
 
-
-    /* 
-    data": [
-    {
-    "date": "2020-04-16",
-    "confirmed": 2673,
-    "deaths": 196,
-    "recovered": 596,
-    "confirmed_diff": 168,
-    "deaths_diff": 13,
-    "recovered_diff": 7,
-    "last_update": "2020-04-16 23:30:31",
-    "active": 1881,
-    "active_diff": 148,
-    "fatality_rate": 0.0733,
-    "region": {
-    "iso": "EGY",
-    "name": "Egypt",
-    "province": "",
-    "lat": "26.8206",
-    "long": "30.8025",
-    "cities": []
-    }
-    }
-    ]
-    
-    
-    */
-
-
-    // document.getElementById("date").innerHTML = "12-9-2025";
-    // document.getElementById("areaName").innerHTML = "Egypt";
-    // document.getElementById("latestBy").innerHTML = "202365";
-    // document.getElementById("deathNew").innerHTML = "985";
-
 }
 fetchData();
