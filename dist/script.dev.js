335dee464f9bdf57a55101d4d0b64151fb4d97ed
hassan khaled <hasan.khaled.fcis@gmail.com>
2025-03-09 17:44:20 +0200

    style(enable_show_when_sucessful) make form visiable after api reqeust is fullfied
     make form hidden when reqeust is successful make it visiable and fill inputs

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var core = document.getElementById("main");
+
 function getCurrentDate() {
   var currentDate = new Date();
   var month = new Intl.DateTimeFormat('en-US', {
@@ -8,7 +10,6 @@ function getCurrentDate() {
   var day = new Intl.DateTimeFormat('en-US', {
     day: '2-digit'
   }).format(currentDate);
-  console.log("Current Date: ".concat(month, "-").concat(day));
   return "".concat(month, "-").concat(day);
 }
 
@@ -18,16 +19,24 @@ function fetchData() {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
+          core.style.visibility = 'hidden';
           date = new Date();
-          _context.next = 3;
+          _context.next = 4;
           return regeneratorRuntime.awrap(fetch("https://covid-api.com/api/reports?date=2021-".concat(getCurrentDate(), "&iso=EGY")));
 
-        case 3:
+        case 4:
           res = _context.sent;
-          _context.next = 6;
+
+          if (!(res != null)) {
+            _context.next = 16;
+            break;
+          }
+
+          core.style.visibility = 'visible';
+          _context.next = 9;
           return regeneratorRuntime.awrap(res.json());
 
-        case 6:
+        case 9:
           record = _context.sent;
           data = record.data[0];
           document.getElementById("country").innerHTML = data.region.name;
@@ -36,7 +45,7 @@ function fetchData() {
           document.getElementById("deathNew").value = data.deaths;
           document.getElementById("confirmed").value = data.confirmed;
 
-        case 13:
+        case 16:
         case "end":
           return _context.stop();
       }
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+
+const core = document.getElementById("main");
 function getCurrentDate() {
 
     const currentDate = new Date();
@@ -6,15 +8,20 @@ function getCurrentDate() {
     const month = new Intl.DateTimeFormat('en-US', { month: '2-digit' }).format(currentDate);
     const day = new Intl.DateTimeFormat('en-US', { day: '2-digit' }).format(currentDate);
 
-    console.log(`Current Date: ${month}-${day}`);
+   
     return `${month}-${day}`
 }
 
 async function fetchData() {
+    core.style.visibility = 'hidden'; 
     const date = new Date();
 
     const res = await fetch(`https://covid-api.com/api/reports?date=2021-${getCurrentDate()}&iso=EGY`);
 
+    
+    if(res !=null){
+        
+        core.style.visibility = 'visible'; 
     const record = await res.json();
 
     const data = record.data[0];
@@ -26,7 +33,9 @@ async function fetchData() {
     document.getElementById("active").value = data.active;
     document.getElementById("deathNew").value = data.deaths;
     document.getElementById("confirmed").value = data.confirmed;
+      
 
+}
 
 }
 fetchData();
